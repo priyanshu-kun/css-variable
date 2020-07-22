c87b2a7d5042bb1500149f3a79926be60a3c9b21
Priyanshu sharma <priyanshushrama709@gmail.com>
2020-07-22 11:01:36 +0530

    Add files via upload

---
+++ Play with css variable/brain.js
@@ -0,0 +1,9 @@
+var inputs = document.querySelectorAll('.control input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
